85d55ad3cfc478063368bf478986ee06bb6d40fa
yangxuxian <53417102+yangxuxian@users.noreply.github.com>
2025-02-05 10:23:04 +0800

    bean

---
+++ bean/WorkListBean.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LiteStarNote.bean
+{
+    internal class WorkListBean
+    {
+        public int Id { get; set; }
+        public string Num { get; set; }
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+        public string State { get; set; }
+        public string Operate { get; set; }
+    }
+}
---
+++ bean/WorkTypeBean.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LiteStarNote.bean
+{
+    internal class WorkTypeBean
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
